2fdd7344fbc914c0c0d13bfea16203eccc3f624a
saequus <slava@spetsyian.com>
2024-12-08 22:32:12 +0100

    [Feat] day 6 upd

--- python/2024/day6.py
+++ python/2024/day6.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=str)
 parser.add_argument('-t', '--task', help='Task number which associated function to execute', type=int)
@@ -15,39 +14,43 @@ DIRECTIONS_NXT_DIR_MAP = {
 
 
 def find_current_position(board):
-    direction = None
-    i = j = cur_i = cur_j = 0
+    i = j = 0
     line_len = len(board[0])
     found = False
     while i < len(board) and not found:
         while j < line_len and not found:
             if board[i][j] == '^':
-                direction = 'top'
                 found = True
-                cur_i, cur_j = i, j
+                return 'top', i, j
             if board[i][j] == '>':
-                direction = 'right'
                 found = True
-                cur_i, cur_j = i, j
+                return 'right', i, j
             if board[i][j] == 'v':
-                direction = 'bottom'
                 found = True
-                cur_i, cur_j = i, j
+                return 'bottom', i, j
             if board[i][j] == '<':
-                direction = 'left'
                 found = True
-                cur_i, cur_j = i, j
+                return 'left', i, j
             j += 1
         j = 0
         i += 1
-    return direction, cur_i, cur_j
+
 
 
 def find_paths(board, direction, i, j):
     visited = {}
+    visited_with_dir = {}
     border_reached = False
     while len(board) > i > 0 and 0 < j < len(board) - 1 and not border_reached:
-        visited[i, j] = ''
+        if (i, j) not in visited:
+            visited[i, j] = {'times': 1}
+            visited_with_dir[i, j, direction] = False
+        else:
+            visited[i, j] = {'times': visited[i, j]['times'] + 1}
+            nxt_dir = DIRECTIONS_NXT_DIR_MAP[direction]
+            if (i, j, nxt_dir) in visited_with_dir:
+                visited_with_dir[i, j, nxt_dir] = True
+
         match direction:
             case 'top':
                 border_reached = i <= 0
@@ -85,26 +88,53 @@ def find_paths(board, direction, i, j):
                     i -= 1
                 else:
                     j -= 1
-    return visited
+    return visited, visited_with_dir
 
 
-def visualize(board: list[str], visited: dict):
+def visualize(board: list[str], visited: dict, visited_with_dir: dict):
     output = ''
     for i in range(len(board)):
         for j in range(len(board[i])):
-            if (i, j) in visited:
-                output += 'X'
-            else:
-                output += board[i][j]
-        output += '\n'
+            marked = False
+            if (i, j, 'top') in visited_with_dir:
+                if visited_with_dir[i, j, 'top']:
+                    output += 'T'
+                    marked = True
+            elif (i, j, 'left') in visited_with_dir:
+                if visited_with_dir[i, j, 'left']:
+                    output += 'L'
+                    marked = True
+            elif (i, j, 'bottom') in visited_with_dir:
+                if visited_with_dir[i, j, 'bottom']:
+                    output += 'B'
+                    marked = True
+            elif (i, j, 'right') in visited_with_dir:
+                if visited_with_dir[i, j, 'right']:
+                    output += 'R'
+                    marked = True
 
+            if (i, j) in visited and not marked:
+                output += 'X'
+            elif not marked:
+                output += board[i][j]
+
+        output += '\n'
+    print(output)
 
 def predict_guard(board: list[str]):
     init_direction, init_i, init_j = find_current_position(board)
-    visited = find_paths(board, init_direction, init_i, init_j)
-    # visualize(board, visited)
+    visited, visited_with_dir = find_paths(board, init_direction, init_i, init_j)
+    # visualize(board, visited, visited_with_dir)
 
     return len(visited.keys())
+
+
+def count_loops(board: list[str]):
+    init_direction, init_i, init_j = find_current_position(board)
+    visited, visited_with_dir = find_paths(board, init_direction, init_i, init_j)
+    # visualize(board, visited, visited_with_dir)
+
+    return sum(visited_with_dir.values())
 
 
 if __name__ == '__main__':
@@ -120,4 +150,6 @@ if __name__ == '__main__':
         print('The guard visited tiles: ', res)
 
     if args.task == 2:
-        pass
+        res = count_loops(lines)
+
+        print('Solution not yet found ^-^: ', res)
