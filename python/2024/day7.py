eda62c84c3ab692b7123dd15e18656d42bb107c2
saequus <slava@spetsyian.com>
2024-12-14 16:58:10 +0100

    [Feat] add day7 task 2

--- python/2024/day7.py
+++ python/2024/day7.py
@@ -19,6 +19,27 @@ def calc(expected: int, params: list[int], index: int) -> bool:
     return False
 
 
+def calc2(expected: int, params: list[int], index: int) -> bool:
+    if index < 0:
+        return False
+    last = params[index]
+    if index == 0:
+        return expected == last
+    if expected % last == 0 and calc2(expected // last, params, index - 1):
+        return True
+    if expected > last and calc2(expected - last, params, index - 1):
+        return True
+
+    expected_as_str = str(expected)
+    lastparam_as_str = str(last)
+
+    if len(expected_as_str) > len(lastparam_as_str) and expected_as_str.endswith(lastparam_as_str):
+        new_target = int(expected_as_str[:-len(lastparam_as_str)])
+        return calc2(new_target, params, index - 1)
+
+    return False
+
+
 if __name__ == '__main__':
     raw = open(args.filename).readlines()
     breakpoint_reached = False
@@ -38,4 +59,13 @@ if __name__ == '__main__':
         print('Total calibration result: ', res)
 
     if args.task == 2:
-        pass
+        res = 0
+        for expected_sum, values in calibration.items():
+            if calc2(expected_sum, values, len(values) - 1):
+                res += expected_sum
+
+        print('Total calibration result with concatenation operator: ', res)
+
+
+
+
