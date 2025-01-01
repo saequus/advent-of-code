c2f96d483b98d7ed15c2ebf723d7b35166d434bd
saequus <slava@spetsyian.com>
2025-01-01 19:43:19 +0100

    [Feat] add 8 day task 2

--- python/2024/day8.py
+++ python/2024/day8.py
@@ -18,6 +18,15 @@ class Antenna:
         self.name = name
         self.position = position
 
+def find_antennas(board, max_row, max_col):
+    antennas = {}
+    for row in range(max_row):
+        for col in range(max_col):
+            value = board[row][col]
+            if value != '.':
+                antennas.setdefault(value, []).append(Antenna(value, Position(row, col)))
+    return antennas
+
 
 def mirror(a, b, radius):
     dx = b.row - a.row
@@ -27,42 +36,60 @@ def mirror(a, b, radius):
     return pos_a, pos_b
 
 
-def print_matrix(anti_nodes, max_rows, max_cols):
-    for row_idx in range(max_rows):
-        for col_idx in range(max_cols):
-            if Position(row_idx, col_idx) in anti_nodes:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-
-
-def find_anti_nodes1(antennas, max_rows, max_cols, anti_nodes):
+def find_anti_nodes1(antennas, max_row, max_col, anti_nodes):
     pair_of_antennas = combinations(antennas, 2)
     for pair in pair_of_antennas:
         a, b = mirror(pair[0].position, pair[1].position, 1)
-        if is_in_boundaries(a.row, a.col, max_rows, max_cols):
+        if is_in_boundaries(a.row, a.col, max_row, max_col):
             anti_nodes[(a.row, a.col)] = True
-        if is_in_boundaries(b.row, b.col, max_rows, max_cols):
+        if is_in_boundaries(b.row, b.col, max_row, max_col):
             anti_nodes[(b.row, b.col)] = True
+
+
+def find_anti_nodes_in_line(antennas, max_row, max_col, anti_nodes):
+    pairs = combinations(antennas, 2)
+    for pair in pairs:
+        radius = 1
+        first_out_of_range = False
+        second_out_of_range = False
+        init_a, init_b = pair[0].position, pair[1].position
+        anti_nodes[(init_a.row, init_a.col)] = True
+        anti_nodes[(init_b.row, init_b.col)] = True
+        while not first_out_of_range or not second_out_of_range:
+            a, b = mirror(init_a, init_b, radius)
+            if is_in_boundaries(a.row, a.col, max_row, max_col):
+                anti_nodes[(a.row, a.col)] = True
+            else:
+                first_out_of_range = True
+            if is_in_boundaries(b.row, b.col, max_row, max_col):
+                anti_nodes[(b.row, b.col)] = True
+            else:
+                second_out_of_range = True
+            radius += 1
 
 
 def calc1(board: list[str]):
     result = 0
-    antennas = {}
-    max_rows = len(board)
-    max_cols = len(board[0])
-    for row in range(max_rows):
-        for col in range(max_cols):
-            value = board[row][col]
-            if value != ".":
-                antennas.setdefault(value, []).append(Antenna(value, Position(row, col)))
+    max_row = len(board)
+    max_col = len(board[0])
+    antennas = find_antennas(board, max_row, max_col)
     anti_nodes = {}
     for sub_antennas in antennas.values():
-        find_anti_nodes1(sub_antennas, max_rows, max_cols, anti_nodes)
+        find_anti_nodes1(sub_antennas, max_row, max_col, anti_nodes)
 
     result += len(anti_nodes)
     return result
+
+def calc2(board: list[str]):
+    anti_nodes = {}
+    max_row = len(board)
+    max_col = len(board[0])
+    antennas = find_antennas(board, max_row, max_col)
+    for sub_antennas in antennas.values():
+        find_anti_nodes_in_line(sub_antennas, max_row, max_col, anti_nodes)
+
+    return len(anti_nodes)
+
 
 
 if __name__ == '__main__':
@@ -77,6 +104,5 @@ if __name__ == '__main__':
         print('Total anti nodes number: ', res)
 
     if args.task == 2:
-        pass
-
-
+        res = calc2(board)
+        print('Total anti nodes number expanding lines: ', res)
